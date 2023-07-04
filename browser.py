6bc81c4b7a33eeecf4c0b58fb90184fe3c4d24ed
Moussa <109428479+Kakaymi10@users.noreply.github.com>
2023-07-04 13:59:54 +0200

    Create browser.py

---
+++ browser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pre4e.org', 80))
+cmd = 'GET http://data.pre4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+  data = mysock.recv(512)
+  if len(data) < 1:
+    break
+  print(data.decode(), end='')
+
+mysock.close()
